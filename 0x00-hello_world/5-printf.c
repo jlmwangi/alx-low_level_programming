202f40b030fafbc8764c7e36c43e3437806a1cd3
mwangi <jlmwangi254@gmail.com>
2023-08-11 01:14:59 +0300

    uses printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main - prints using the printf functiois
- * return=0 if successful
+ * Main - prints, 'with proper grammar, but the outcome is a piece of art
+ * Return: 0 if successful
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
