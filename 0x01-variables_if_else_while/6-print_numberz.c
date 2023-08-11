7d40e2534e8cd580b656956103a4e04153e427de
mwangi <jlmwangi254@gmail.com>
2023-08-12 02:37:40 +0300

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int a;
 
-		for (a = 0; a < 10; a++);
-			{
-				putchar(a);
-			}
-		putchar('\n');
-		return (0);
+	for (a = 0; a < 10; a++);
+		{
+			putchar(a);
+		}
+	putchar('\n');
+	return (0);
 }
 
