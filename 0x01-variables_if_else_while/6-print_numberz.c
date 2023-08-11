434094d28154cf468d3d8e31a967a3ad66d4f122
mwangi <jlmwangi254@gmail.com>
2023-08-12 02:18:35 +0300

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - presents entry information
+ *
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int a = 0
+
+		for (a = 0; a < 10; a++)
+		{
+			putchar(a);
+		}
+		putchar("\n");
+		return (0);
+}
+
