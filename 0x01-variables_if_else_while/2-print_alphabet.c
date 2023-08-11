85ff3d6bbd1131d814d5675ed7d0836200fa59fa
mwangi <jlmwangi254@gmail.com>
2023-08-12 00:48:31 +0300

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	char 'ch' = 'a';
+
+		while (ch <= 'z')
+		{
+			putchar(ch);
+			ch++;
+		}
+	putchar('\n');
+	return (0);
+}
