05049fa9c87d571bec3dfb59c1824976984853a2
mwangi <jlmwangi254@gmail.com>
2023-08-12 02:57:50 +0300

    print in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio>
+/**
+ * main - entry information
+ *
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int z
+
+		for (z = 25; z >= 'a'; z--)
+		{
+			putchar(z);
+		}
+	putchar('\n');
+		return (0);
+}
