1cfab1c2f1d8ca155190791d5a9a3a68592dcb41
mwangi <jlmwangi254@gmail.com>
2023-08-12 01:35:42 +0300

    print lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - presents the entry point
+ *
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	char ch = 'a'
+
+		for (ch = 'a', ch <= 'z', ch++)
+		{
+			if (ch != 'e' && ch != 'q')
+			{
+				putchar(ch);
+			}
+		}
+	putchar('\n');
+	return (0);
+}
