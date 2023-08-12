e0e35b7fb96882e850ab11763f4a69eba07a9acd
mwangi <jlmwangi254@gmail.com>
2023-08-12 04:47:37 +0300

    prints possible combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints single combinations of numbers
+ *
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int n;
+
+		for (n = 0; n < 10; n++)
+		{
+			putchar(n);
+			if (n !=9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	putchar('\n');
+	return (0);
+}
