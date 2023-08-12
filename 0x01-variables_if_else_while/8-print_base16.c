b36f1488d3859ac9be81abf36277bdad0cbb6977
mwangi <jlmwangi254@gmail.com>
2023-08-12 03:27:57 +0300

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int n = 0;
+	char letter = a;
+
+	for (int n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	for (char letter = a; letter <= f; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+		return (0);
+}
