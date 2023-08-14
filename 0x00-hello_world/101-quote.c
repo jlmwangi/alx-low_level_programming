6ba792e2736d35e1910e739ab573766aa41d8752
mwangi <jlmwangi254@gmail.com>
2023-08-14 16:50:12 +0300

    print

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - markts the entry point
+ *
+ * Return: 1 (successful)
+ */
+int main(void)
+{
+	printf(""and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	return (1);
+}
