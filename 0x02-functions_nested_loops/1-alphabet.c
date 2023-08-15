70afb7f46525cdf252dcc741cdf4513d2f71e64e
mwangi <jlmwangi254@gmail.com>
2023-08-15 23:25:47 +0300

    print putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+/**
+ * main - presents the entry information
+ *
+ * Return; 0(successful)
+ */
+int main(void)
+{
+	for (int c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
