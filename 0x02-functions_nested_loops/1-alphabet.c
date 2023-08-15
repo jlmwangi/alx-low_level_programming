c6ccb0f4096c6980cd4204949fef9ca9a0c5c5de
mwangi <jlmwangi254@gmail.com>
2023-08-15 23:58:17 +0300

    print putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,16 +5,13 @@
  * Return; 0(successful)
  */
 void print_alphabet(void);
-({(
+
+int main(void)                                                                                                          {                                                                                                                               print_alphabet();                                                                                                       putchar('\n');                                                                                                          return 0;                                                                                                       }           
+
+void print_alphabet(void)
+{
 	for (int c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
-})
-
-int main(void)
-{
-	print_alphabet();
-	putchar('\n');
-	return 0;
 }
