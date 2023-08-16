d37af6c6605575839d3513cb5d8420668c3e1614
mwangi <jlmwangi254@gmail.com>
2023-08-17 02:31:40 +0300

    add integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -12,22 +12,8 @@ int add(int a, int b)
 {
 	int x;
 
-		for (a = 0; a > 0; a++)
-			if (a > 0)
-				_putchar(a);
-			else if (a < 0)
-				_putchar(a);
-			else
-				_putchar(0);
-		for (b = 0; b > 0; b++)
-			if (b > 0)
-				_putchar(b);
-			else if (b < 0)
-				_putchar(b);
-			else
-				_putchar(0);
-		x =(a + b);
-			_putchar(x + '0');
-			_putchar('\n');
-		return (x);
+	x = (a + b);
+		_putchar(x + '0');
+		_putchar('\n');
+	return (x);
 }
