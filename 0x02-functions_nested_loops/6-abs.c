f003713ca77f3f1edba1d6bcb161c6170b7da07c
mwangi <jlmwangi254@gmail.com>
2023-08-16 21:05:47 +0300

    compute the absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,22 +3,19 @@
 /**
  * _abs - computes the absolute value of an integer
  *
- * int - represents integer
- * n - represents integer
+ * int: represents integer
+ *
+ * @n: represents integer
  *
  * Return: 0(successful)
  */
 
 int _abs(int n)
 {
-	int n;
-
 	if (n < 0)
-	{
-		_putchar('|n|');
-	}
-	else
 	{
 		_putchar('n');
 	}
+	_putchar('\n');
+	return (0);
 }
