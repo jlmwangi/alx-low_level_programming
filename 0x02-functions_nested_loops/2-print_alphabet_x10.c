2db5ea8509f256fc58d81b6ab8ee4b109b1047ff
mwangi <jlmwangi254@gmail.com>
2023-08-16 16:42:58 +0300

    print ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,7 @@
 /**
  * print_alphabet_x10 - prints the alphabet 10 times
  *
- * return: 0(successful)
- */
+  */
 
 void print_alphabet_x10(void)
 {
@@ -11,13 +10,13 @@ void print_alphabet_x10(void)
 
 	for (i = 1; i <= 10; i++)
 	{
-		char c = 'a'
+		char c = 'a';
 
 		while (c <= 'z')
 		{
 			_putchar(c);
 			c++;
 		}
-		putchar('\n');
 	}
+	_putchar('\n');
 }
