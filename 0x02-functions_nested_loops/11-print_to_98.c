c79283e523d38d5ff6cdd18f99baacd703f40442
mwangi <jlmwangi254@gmail.com>
2023-08-17 04:11:09 +0300

    print natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,21 +9,19 @@ void print_to_98(int n)
 
 	if (i <= 98)
 	{
-		for (i = n; i <= 98; i++)
-			printf("%d", i);
-		if (i != 98)
-		{
-			printf(", ");
-		}
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d", i);
+		printf(", ");
+	}
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
-			printf("%d", i);
-		if (i != 98)
-		{
-			printf(", ");
-		}
+	for (i = n; i >= 98; i--)
+	{
+		printf("%d", i);
+		printf(", ");
+	}
 	}
 	printf("\n");
 }
