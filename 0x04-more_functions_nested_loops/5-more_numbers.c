22e9ab933e781ba6ac23f83cc3547e4bf504a0ce
mwangi <jlmwangi254@gmail.com>
2023-08-18 01:28:38 +0300

    prints ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - prints ten times the numbers
+ *
+ * return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int x;
+
+	for (i = 0; i <= 9; i++)
+	{
+		x = 0;
+		while (x <= 14)
+		{
+			_putchar(x + '0');
+			x++;
+		}
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
+
