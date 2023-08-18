880a3d63194bc38782943121ac0b7fd68f2776f7
mwangi <jlmwangi254@gmail.com>
2023-08-18 12:01:36 +0300

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_square - prints squares
+ *
+ * @size: size of the square.
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('#');
+	}
+}
