2de158af2413bf193967b0b9376dd8235a0c0ed9
mwangi <jlmwangi254@gmail.com>
2023-08-22 00:11:21 +0300

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_rev - prints a function in revers
+ * @s: points to the stringe
+ *
+ * return: void
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+	int a;
+	char *start, *finish, ch;
+
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+
+	start = s;
+	finish = s + l - 1;
+	for (a = 0; a < l / 2; a++)
+	{
+		ch = *finish;
+		*finish = *start;
+		*start = ch;
+		start++;
+		finish--;
+	}
+}
+
