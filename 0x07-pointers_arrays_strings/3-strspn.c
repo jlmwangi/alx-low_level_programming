27c52c3b6b9d21367baef4411db68f9e17647024
mwangi <jlmwangi254@gmail.com>
2023-08-28 18:47:16 +0300

    find substring length

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strspn - gets length of prefix substring
+ * @s: represents string
+ * @accept: string containing accept characters
+ *
+ * Return: int l
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int l = 0;
+
+	while (*s != '\0' && *s == *accept)
+	{
+		l++;
+		s++;
+		accept++;
+	}
+	return (l);
+}
