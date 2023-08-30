ef51ba586b8ac3f2934a96d4695e56acdb76fcd0
mwangi <jlmwangi254@gmail.com>
2023-08-30 22:25:45 +0300

    check prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,16 +9,11 @@ int _is_prime_number(int n)
 {
 	int i;
 
-	i = 0;
-	if (i <= n)
-	{
-		i++;
-	}
 	if (n <= 1)
 	{
 		return (0);
 	}
-	if (i == 1)
+	if (i * i > n)
 	{
 		return (1);
 	}
@@ -26,5 +21,5 @@ int _is_prime_number(int n)
 	{
 		return (0);
 	}
-	return (n);
+	return (_is_prime_number(n, i + 1));
 }
