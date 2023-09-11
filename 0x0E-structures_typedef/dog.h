314cc1aa120c5638c15b7afdb42f6850a53f4fc1
mwangi <jlmwangi254@gmail.com>
2023-09-12 00:16:22 +0300

    define a new type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - description on a new struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif /* DOG */
