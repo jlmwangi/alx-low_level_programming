269a2f084690d02ce23e755d9a176545dda9d88e
mwangi <jlmwangi254@gmail.com>
2023-09-11 18:55:53 +0300

    print file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define FILE_NAME (__FILE__)
+
+/**
+ * main - prints macro
+ *
+ * Return: void
+ */
+int main(void)
+{
+	printf("%s\n", FILE_NAME);
+	return (0);
+}
