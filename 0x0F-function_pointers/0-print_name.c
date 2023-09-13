4ab60d4316a9d922a09ac7a30234a02688fe1691
mwangi <jlmwangi254@gmail.com>
2023-09-14 00:17:41 +0300

    print a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "function_pointers.h"
 /**
  * print_name - prints a name
  * @name: name to be printed
@@ -7,5 +8,4 @@
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
-	printf("%s\n", name);
 }
