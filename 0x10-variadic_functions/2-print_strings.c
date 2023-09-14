6eea15b3c225e406aac1b7c4d00c3717f998d790
mwangi <jlmwangi254@gmail.com>
2023-09-14 15:51:57 +0300

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,11 +12,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list x;
 
-	if (separator == NULL)
-	{
-		return;
-	}
-
 	va_start(x, n);
 
 	for (i = 0; i < n; i++)
@@ -31,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", str);
 		}
-		if (i < n - 1)
+		if (i < n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
