9b641b51afd3dc8a4f30615f49c9ac2bdee00172
mwangi <jlmwangi254@gmail.com>
2023-09-14 21:31:45 +0300

    print anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,15 +4,51 @@
 /**
  * print_all - prints all data types
  * @format: various data types
- *
  */
 void print_all(const char * const format, ...)
 {
 	va_list x;
+	int i;
 
 	va_start(x, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (i > 0)
+		{
+			printf(", ");
+		}
+		if (format[i] == 'c')
+		{
+			char c = va_arg(x, int);
 
-	vprintf(format, x);
+			printf("%c", c);
+		}
+		else if (format[i] == 'd')
+		{
+			int n = va_arg(x, int);
+
+			printf("%i", n);
+		}
+		else if (format[i] == 'f')
+		{
+			double f = va_arg(x, double);
+
+			printf("%f", f);
+		}
+		else if (format[i] == 's')
+		{
+			char *s = va_arg(x, char *);
+
+			if (s == NULL)
+			{
+				printf("(nil)");
+			}
+			else
+			{
+				printf("%s", va_arg(x, char *));
+			}
+		}
+	}
 	va_end(x);
 	printf("\n");
 }
