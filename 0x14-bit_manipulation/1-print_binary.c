742954d78beb15c8517370d79cdf1b2dc261ab5b
Mwangii <jlmwangi254@gmail.com>
2023-10-20 11:42:40 +0300

    print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,16 +7,23 @@
  */
 void print_binary(unsigned long int n)
 {
-	char s;
+	unsigned long int i;
 	int c;
 	int j;
 
-	c = sizeof(unsigned long int) * 8;
+	c = 0;
 
-	for (j = c - 1; j >= 0; j--)
+	for (j = 63; j >= 0; j--)
 	{
-		s = ((n >> j) & 1) + '0';
-		putchar(s);
+		i = n >> j;
+		if (i & 1)
+		{
+			_putchar('1');
+			c++;
+		}
+		else if (c)
+			_putchar('0');
+		if (!c)
+			_putchar('0');
 	}
-	putchar('\n');
 }
