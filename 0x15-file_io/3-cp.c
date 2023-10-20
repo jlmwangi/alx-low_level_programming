c3a9ab41d0bd793a3abf57f9da07ed3096a885ca
Mwangii <jlmwangi254@gmail.com>
2023-10-20 13:10:52 +0300

    debugging

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,41 @@
 #include "main.h"
+char *create_buf(char *file);
+void close_file(int fd);
+
+/**
+ * create_buf - creates buffer
+ * @file: name of the file
+ *
+ * Return: pointer to buffer
+ */
+char *create_buf(char *file)
+{
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+
+	if (buf == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (buf);
+}
+/**
+ * close - close file descriptors
+ * @fd: file descriptor
+ */
+void close_file(int fd)
+{
+	int n;
+
+	n = close(fd);
+	if (n == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
 /**
  * main - presents entry point
  * @argc: argument count
@@ -8,10 +45,8 @@
  */
 int main(int argc, char *argv[])
 {
-	const char *file_from = argv[1];
-	const char *file_to = argv[2];
 	int fd_from, fd_to;
-	char buffer[1024];
+	char *buf;
 	ssize_t r, w;
 
 	if (argc != 3)
@@ -19,44 +54,31 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
+	buf = create_buf(argv[2]);
+	fd_from = open(argv[1], O_RDONLY);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	r = read(fd_from, buf, 1024);
+
+	do {
+	if (fd_from == -1 || r == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buf);
 		exit(98);
 	}
-
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd_to == -1)
+	w = write(fd_to, buf, r);
+	if (fd_to == -1 || w == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
+		free(buf);
 		exit(99);
 	}
-	while ((r = read(fd_from, buffer, 1024)) > 0)
-	{
-		w = write(fd_to, buffer, r);
-		if (w == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+	r = read(fd_from, buf, 1024);
+	fd_to = open(argv[2], O_WRONLY | O_APPEND);
+	} while (r > 0);
+	free(buf);
+	close(fd_from);
+	close(fd_to);
 
-	}
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
 	return (0);
 }
