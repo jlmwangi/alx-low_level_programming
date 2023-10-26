56a1df006ecbd1f70f18357d7f66b740e97ccb4c
Mwangii <jlmwangi254@gmail.com>
2023-10-26 19:26:54 +0300

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_binary - prints binary representation
+ * @n: long int
+ */
+void print_binary(unsigned long int n)
+{
+	int i;
+	int s;
+	unsigned int j;
+
+	s = sizeof(unsigned int) * 8;
+
+	if (n == 0)
+		_putchar('0');
+
+	for (i = s - 1; i >= 0; i--)
+	{
+		j = 1 << i;
+		_putchar((n & j) ? '1' : '0');
+	}
+}
